4b81a157d4da3be8787fee6573e1aa80a154933a
Ajay Nandurkar <nandurkarajay07@gmail.com>
2025-04-06 18:33:26 +0530

    Added data ingestion pipeline and artifacts directory creation

--- src/logger.py
+++ src/logger.py
@@ -2,20 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-# Generate log filename based on the current timestamp
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# Correct log directory path
-logs_dir = os.path.join(os.getcwd(), "logs")
-os.makedirs(logs_dir, exist_ok=True)  # Create 'logs' folder if it doesn't exist
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-# Correct log file path
-LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
-
-# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(levelname)s [%(filename)s:%(lineno)d] - %(message)s",
-    level=logging.INFO
-)
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 
+
+)
